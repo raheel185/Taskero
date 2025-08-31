b6fe28b653c05392e691f177f77e313ac00b6fb3
Raheel Fida <diego.mandu.deve@gmail.com>
2025-08-31 16:34:32 +0500

    Update

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Header Nav</h1>
-    </div>
+    <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center sticky top-0 z-50">
+      {/* Logo / Brand */}
+      <div>
+        <Link to="/" className="text-2xl font-bold text-blue-600 hover:text-blue-800">
+          Taskero
+        </Link>
+      </div>
+
+      {/* Navigation Links */}
+      <div className="flex gap-6">
+        <Link 
+          to="/" 
+          className="text-gray-700 hover:text-blue-600 transition duration-200"
+        >
+          Home
+        </Link>
+        <Link 
+          to="/login" 
+          className="text-gray-700 hover:text-blue-600 transition duration-200"
+        >
+          Login
+        </Link>
+      </div>
+    </nav>
   )
 }
 
