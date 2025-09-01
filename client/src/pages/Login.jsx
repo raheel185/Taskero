6bbe4d05f058d424430accd78d356c299c3e1e95
Raheel Fida <diego.mandu.deve@gmail.com>
2025-09-01 10:34:50 +0500

    Update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <h1>Login Page</h1>
+      <h1>User Login</h1>
+
+      <form>
+          <input type="email"  placeholder='Enter your email'/>
+          <input type="password"  placeholder='***********'/>
+          <input type="submit" value="Login"/>
+      </form>
+
     </div>
   )
 }
