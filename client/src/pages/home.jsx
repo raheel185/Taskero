beeca7f36e498d8e1375269271989bbeaefe6eb7
Raheel Fida <diego.mandu.deve@gmail.com>
2025-09-01 05:58:47 +0500

    Update

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="flex justify-between items-center">
+      
+      <div>
+        <h1>Your Smart Task Management Solution</h1>
+        <p>Taskero helps you manage your daily tasks, set priorities, and stay focused — all in one place.</p>
+        <a href="#">Start Managing Tasks</a>
+      </div>
+
+      <div>
+
+      </div>
+
     </div>
   )
 }
