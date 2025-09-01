07d4b0b7a30606298efc8c387bc1d81b5797eeb6
Raheel Fida <diego.mandu.deve@gmail.com>
2025-09-01 10:39:04 +0500

    Update

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+  const [email, setEmail] = useState('')
+
+
+  async function handleLogin(e){
+
+    e.preventDefault()
+
+    console.log(email)
+  }
+
   return (
     <div>
       <h1>User Login</h1>
 
-      <form>
-          <input type="email"  placeholder='Enter your email'/>
+      <form onSubmit={handleLogin}>
+          <input onChange={(e)=>setEmail(e.target.value)} type="email"  placeholder='Enter your email'/>
           <input type="password"  placeholder='***********'/>
           <input type="submit" value="Login"/>
       </form>
