ec785125e1fd7291ca9aca291c02e5a2d895f824
Raheel Fida <diego.mandu.deve@gmail.com>
2025-09-14 06:00:47 +0500

    Server update

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect MongoDb
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server is running"));
